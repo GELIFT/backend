a39bf74d04dc4b2731da370860f0c49064b49868
Geert Derks <g.a.f.derks@student.tue.nl>
2018-10-04 20:34:59 +0200

    Remove allowed hosts from settings file

--- GELIFT/settings.py
+++ GELIFT/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY') is imported with django_heroku locals
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -31,7 +31,7 @@ DEBUG = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+#ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 GMAPS_API_KEY = os.environ.get('GMAPS_API_KEY')
 FIREBASE_KEY = os.environ.get('FIREBASE_KEY')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
